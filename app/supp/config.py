3c526cf9fb4b93606fe6b722ec0f5a227a1af615
Timo Mäkinen <timo.makinen@tuni.fi>
2024-01-08 12:15:07 +0200

    fix prompt

--- app/supp/config.py
+++ app/supp/config.py
@@ -1,13 +1,15 @@
 
 from argparse import ArgumentParser
 
+ASSIGNMENT_ID = 'ex01a'
+
 todo = {}
 
 
 def _get_arguments():
 
     parser = ArgumentParser(
-        description='ex1.a'
+        description=ASSIGNMENT_ID
     )
     parser.add_argument(
         '-r', '--review', choices=['0', '1', '2'], default='0',
@@ -36,4 +38,4 @@ def set_config():
 
     todo['queries'] = queries  
     todo['folder'] = _get_todo_folder(args)
-    todo['prompt'] = f'ex1.a [{todo["folder"]}] > '
+    todo['prompt'] = f'{ASSIGNMENT_ID} [{todo["folder"]}] > '
